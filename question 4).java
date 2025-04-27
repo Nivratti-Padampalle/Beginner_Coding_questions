303b49da093309051f1f81d7be855c4fcca1a075
Nivratti-Padampalle <padampallenivratti@gmail.com>
2025-04-27 13:59:26 +0530

    Add files via upload
    palindrome num or not

---
+++ question 4).java
@@ -0,0 +1,33 @@
+
+You are given an integer n. Your task is to determine whether it is a palindrome.
+
+A number is considered a palindrome if it reads the same backward as forward, like the string examples "MADAM" or "MOM".
+
+Examples:
+
+Input: n = 555
+Output: true
+Explanation: The number 555 reads the same backward as forward, so it is a palindrome.
+Input: n = 123
+Output: false
+Explanation: The number 123 reads differently backward (321), so it is not a palindrome.
+Input: n = 1221
+Output: true
+Ans:
+class Solution {
+    public boolean isPalindrome(int n) {
+        // Code here
+        String rev="";
+        int org=n;
+        while(n > 0){
+            int digit=n % 10;
+            rev+=digit;
+            n= n /10;
+        }
+        int number=Integer.parseInt(rev);
+        if(number == org){
+            return true;
+        }
+        return false;
+    }
+}
