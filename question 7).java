819261633b7921eff7e3d70dfc08ba4570e95d1a
Nivratti-Padampalle <padampallenivratti@gmail.com>
2025-04-28 10:16:00 +0530

    Add files via upload

---
+++ question 7).java
@@ -0,0 +1,24 @@
+Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
+
+Note: In case of multiple occurrences of ceil of x, return the index of the last occurrence.
+
+Examples
+
+Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 5
+Output: 1
+Explanation: Largest number less than or equal to 5 is 2, whose index is 1.
+
+Ans:
+class Solution {
+
+    static int findFloor(int[] arr, int x) {
+       
+       int large=-1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= x){
+                large =i;
+            }
+        }
+        return large;
+    }
+}
