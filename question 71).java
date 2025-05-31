0d316ddcacba83cf3b56c2baa35017a0382f0153
Nivratti-Padampalle <padampallenivratti@gmail.com>
2025-05-31 18:21:24 +0530

    Add files via upload

---
+++ question 71).java
@@ -0,0 +1,40 @@
+You are given a 0-indexed integer array nums and an integer k. Your task is to perform the following operation exactly k times in order to maximize your score:
+
+Select an element m from nums.
+Remove the selected element m from the array.
+Add a new element with a value of m + 1 to the array.
+Increase your score by m.
+Return the maximum score you can achieve after performing the operation exactly k times.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5], k = 3
+Output: 18
+Explanation: We need to choose exactly 3 elements from nums to maximize the sum.
+For the first iteration, we choose 5. Then sum is 5 and nums = [1,2,3,4,6]
+For the second iteration, we choose 6. Then sum is 5 + 6 and nums = [1,2,3,4,7]
+For the third iteration, we choose 7. Then sum is 5 + 6 + 7 = 18 and nums = [1,2,3,4,8]
+So, we will return 18.
+It can be proven, that 18 is the maximum answer that we can achieve.
+ans:
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        int sum = 0;
+        int index = 0;
+        while (k > 0) {
+            int max = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] > max) {
+                    max = nums[i];
+                    index = i;
+                }
+            }
+            sum = sum + max;
+            nums[index] = max + 1;
+            k--;
+        }
+        return sum;
+    }
+}
