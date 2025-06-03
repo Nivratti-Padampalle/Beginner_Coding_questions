70637afe41b354575136f1c4c61dd8c55a9ee85b
Nivratti-Padampalle <padampallenivratti@gmail.com>
2025-06-03 09:24:00 +0530

    Add files via upload

---
+++ question 77).java
@@ -0,0 +1,36 @@
+Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+To separate the digits of an integer is to get all the digits it has in the same order.
+
+For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+ 
+
+Example 1:
+
+Input: nums = [13,25,83,77]
+Output: [1,3,2,5,8,3,7,7]
+Explanation: 
+- The separation of 13 is [1,3].
+- The separation of 25 is [2,5].
+- The separation of 83 is [8,3].
+- The separation of 77 is [7,7].
+answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order
+ans:
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            String str = Integer.toString(nums[i]);
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                int num = Character.getNumericValue(ch);
+                list.add(num);
+            }
+        }
+        int ans[] = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
