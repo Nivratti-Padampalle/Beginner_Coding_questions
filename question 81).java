016ba7b1a8a075dd7dbe717f3b098e3b301c3064
Nivratti-Padampalle <padampallenivratti@gmail.com>
2025-06-06 13:16:31 +0530

    Add files via upload

---
+++ question 81).java
@@ -0,0 +1,41 @@
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+ 
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+ans:
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            boolean ispresent = true;
+            for (int j = 0; j < words[i].length(); j++) {
+                char ch = words[i].charAt(j);
+                if (allowed.indexOf(ch) == -1) {
+                    ispresent = false;
+                    break;
+                }
+            }
+            if (ispresent) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
