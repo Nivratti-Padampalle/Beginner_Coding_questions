35ce6236b79385193a622f23da75e268366a4f7f
Nivratti-Padampalle <padampallenivratti@gmail.com>
2025-08-05 09:26:42 +0530

    Add files via upload

---
+++ 1679. Max Number of K-Sum Pairs.java
@@ -0,0 +1,66 @@
+1679. Max Number of K-Sum Pairs
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+ans:
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        int Total_Operations = 0;
+        int start = 0;
+        int end = nums.length - 1;
+        Arrays.sort(nums);
+        while (start < end) {
+            if (nums[start] + nums[end] == k) {
+                Total_Operations++;
+                start++;
+                end--;
+            } else if (nums[start] + nums[end] < k) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        /*
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (!set.contains(nums[i]) && !set.contains(nums[j])) {
+                    if (nums[i] + nums[j] == k) {
+                        set.add(nums[i]);
+                        set.add(nums[j]);
+                        Total_Operations++;
+                        break;
+                    }
+                }
+            }
+        }
+        */
+        return Total_Operations;
+    }
+}
