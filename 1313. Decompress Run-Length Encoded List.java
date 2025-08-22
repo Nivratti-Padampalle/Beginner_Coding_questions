7dff91f1da034ebfae66e5d7ba39db6f66127e8b
Nivratti-Padampalle <padampallenivratti@gmail.com>
2025-08-22 08:11:53 +0530

    Add files via upload

---
+++ 1313. Decompress Run-Length Encoded List.java
@@ -0,0 +1,44 @@
+1313. Decompress Run-Length Encoded List
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+We are given a list nums of integers representing a list compressed with run-length encoding.
+
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+Return the decompressed list.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [2,4,4,4]
+Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
+The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+Example 2:
+
+Input: nums = [1,1,2,3]
+Output: [1,3,3]
+ans:
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i = i + 2) {
+            int frq = nums[i];
+            int val = nums[i + 1];
+            for (int j = 0; j < frq; j++) {
+                result.add(val);
+            }
+        }
+        int ans[] = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
+    }
+}
